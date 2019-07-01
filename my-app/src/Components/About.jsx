bbf11de3c0f9b989821abf0535e700fa2d47cdf9
Jakob <jako2889@stud.kea.dk>
2019-07-01 02:36:26 +0200

    Design stuff

--- my-app/src/Components/About.jsx
+++ my-app/src/Components/About.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { TweenMax, Back, TimelineLite } from "gsap";
 import "../css/About.scss";
+import profile_picture from "../Assets/jakob.jpg";
 
 export class About extends Component {
     constructor(props) {
@@ -42,8 +43,26 @@ export class About extends Component {
       }
     render() {
         return (
-            <div className="wrapper" ref={div => (this.myIntro = div)}>
-                <h1>Welcome!</h1>
+            <div className="wrapper_about" ref={div => (this.myIntro = div)}>
+                <div className="container_about">
+                <div className="intro">
+                    <div className="text">
+                    <h1>HI. NICE TO MEET YOU!</h1>
+                    <p>I'm a multimedia designer and always learning web developer.
+                    I like creating beautiful and unique websites and digital content! Below you can see a little about me and some projects i've been doing.</p>
+                    <div className="skills">
+                    <h3>Skills</h3>
+                    <div className="javascript">Javascript</div>
+                    <div className="Adobe">Adobe</div>
+                    <div className="React">React</div>
+                    <div className="OtherTechTools">Other Tech Tools</div>
+                    </div>
+                    </div>
+                    <img src={profile_picture} alt={profile_picture}/>
+                    <div className="scroll-down"></div>    
+                </div>
+                <div className="projects">
+                <h2>Projects i've been doing.</h2>    
                 <ul>
                     {this.ArrayElements.map((element, index) => <li
                         className={element.class}
@@ -53,6 +72,8 @@ export class About extends Component {
                         {element.name}
                     </li>)}
                 </ul>
+                </div>
+                </div>
             </div>
         )
     }
